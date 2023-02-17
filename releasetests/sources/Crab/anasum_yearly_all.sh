db78d42e9b3b130585f439c26f5a1fb7acedf698
GernotMaier <gernot.maier@desy.de>
2023-02-17 14:30:52 +0100

    plotting refinement

--- releasetests/sources/Crab/anasum_yearly_all.sh
+++ releasetests/sources/Crab/anasum_yearly_all.sh
@@ -13,7 +13,8 @@ fi
 RPARA=${1}
 COM=${2}
   
-for N in "0.5deg" ""  LZE_0.5deg MZE_0.5deg SZE_0.5deg SZE MZE LZE WOBBLE
+# for N in "0.5deg" ""  LZE_0.5deg MZE_0.5deg SZE_0.5deg SZE MZE LZE WOBBLE
+for N in "0.5deg" ""  LZE_0.5deg MZE_0.5deg SZE_0.5deg
 do
    for R in IGNOREACCEPTANCE
    do 
--- releasetests/sources/Crab/plot_all.sh
+++ releasetests/sources/Crab/plot_all.sh
@@ -1,17 +1,21 @@
 # run all plotting scripts for all zenith angle ranges
 #
+
 if [[ $# < 1 ]]; then
-   echo "$0 <runparameter file>"
-   echo ""
-   exit
+echo "
+   $0 <runparameter file>
+   
+"
+exit
 fi
 
 BCKMODEL="IGNOREACCEPTANCE"
 
-for Z in SZE MZE LZE WOBBLE LZE_0.5deg MZE_0.5deg SZE_0.5deg
+# for N in "0.5deg" ""  LZE_0.5deg MZE_0.5deg SZE_0.5deg SZE MZE LZE WOBBLE
+for N in "0.5deg" ""  LZE_0.5deg MZE_0.5deg SZE_0.5deg
 do
-   root -q -l -b "plot_energy_spectra.C(\"${1}\", \"$Z\", \"$BCKMODEL\" )"
-   root -q -l -b "plot_lightcurves.C(\"${1}\", \"$Z\", \"$BCKMODEL\" )"
-   root -q -l -b "plot_skymaps.C(\"${1}\", \"$Z\", \"$BCKMODEL\" )"
-#   root -q -l -b "plot_skymaps.C(\"${1}\", \"$Z\", \"RB\" )"
+   root -q -l -b "plot_energy_spectra.C(\"${1}\", \"$N\", \"$BCKMODEL\" )"
+   root -q -l -b "plot_lightcurves.C(\"${1}\", \"$N\", \"$BCKMODEL\" )"
+   root -q -l -b "plot_skymaps.C(\"${1}\", \"$N\", \"$BCKMODEL\" )"
+#   root -q -l -b "plot_skymaps.C(\"${1}\", \"$N\", \"RB\" )"
 done
