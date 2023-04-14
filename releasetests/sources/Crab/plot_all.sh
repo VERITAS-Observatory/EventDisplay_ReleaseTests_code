ab9161fe74446a7c1d0337d5be93235606a57a5c
GernotMaier <gernot.maier@desy.de>
2023-04-14 13:21:55 +0200

    Crab parameters

--- releasetests/sources/Crab/plot_all.sh
+++ releasetests/sources/Crab/plot_all.sh
@@ -11,8 +11,7 @@ fi
 
 BCKMODEL="IGNOREACCEPTANCE"
 
-# for N in "0.5deg" ""  LZE_0.5deg MZE_0.5deg SZE_0.5deg SZE MZE LZE WOBBLE
-for N in "0.5deg" LZE_0.5deg MZE_0.5deg SZE_0.5deg
+for N in "0.5deg" ""  LZE_0.5deg MZE_0.5deg SZE_0.5deg SZE MZE LZE WOBBLE
 do
    root -q -l -b "plot_energy_spectra.C(\"${1}\", \"$N\", \"$BCKMODEL\" )"
    root -q -l -b "plot_lightcurves.C(\"${1}\", \"$N\", \"$BCKMODEL\" )"
