5df93385ba3e324806ff0245e85c6c335e0b4e0e
GernotMaier <gernot.maier@desy.de>
2023-04-20 16:33:15 +0200

    correct parameter space

--- releasetests/sources/Crab/anasum_yearly_all.sh
+++ releasetests/sources/Crab/anasum_yearly_all.sh
@@ -13,8 +13,7 @@ fi
 RPARA=${1}
 COM=${2}
   
-# for N in "0.5deg" ""  LZE_0.5deg MZE_0.5deg SZE_0.5deg SZE MZE LZE WOBBLE
-for N in "0.5deg" LZE_0.5deg MZE_0.5deg SZE_0.5deg SZE MZE LZE WOBBLE
+for N in LZE_0.5deg MZE_0.5deg SZE_0.5deg SZE MZE LZE WOBBLE
 do
    for R in IGNOREACCEPTANCE
    do 
