87692a7bb76d9e7a9ce460937b90215ee6adf7ae
GernotMaier <gernot.maier@desy.de>
2023-10-19 09:55:00 +0200

    add cuts

--- releasetests/sources/ANALYSIS.v2dl5.Crab.sh
+++ releasetests/sources/ANALYSIS.v2dl5.Crab.sh
@@ -1,8 +1,9 @@
 # Run V2DL5 analysis for all Crab runs
 
+CUT="soft2tel"
 RLISTS=$(ls -1 Crab/runlists/*.dat)
-DL3DIR="/lustre/fs24/group/veritas/shared/processed_data_v490/AP/dl3_pointlike_moderate2tel/"
-DDIR="$VERITAS_USER_DATA_DIR/analysis/Results/v490/AP/v2dl5"
+DL3DIR="/lustre/fs24/group/veritas/shared/processed_data_v490/AP/dl3_pointlike_${CUT}/"
+DDIR="$VERITAS_USER_DATA_DIR/analysis/Results/v490/AP/v2dl5/${CUT}"
 TARGET="Crab"
 CONFIG="ANALYSIS.v2dl5.reflected_region.yml"
 
