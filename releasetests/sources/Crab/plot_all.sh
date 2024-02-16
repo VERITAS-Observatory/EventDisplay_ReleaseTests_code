c7059e85dd38da910d9c380f1536b1f1ac3464f0
GernotMaier <gernot.maier@desy.de>
2024-02-16 15:30:27 +0100

    simplifications

--- releasetests/sources/Crab/plot_all.sh
+++ releasetests/sources/Crab/plot_all.sh
@@ -4,22 +4,43 @@
 
 if [[ $# -lt 1 ]]; then
 echo "
-   $0 <runparameter file>
+   $0 <anasum directory> <output directory>
 
 "
 exit
 fi
 
-BCKMODEL="IGNOREACCEPTANCE"
-PSPACE="SZE_0.5deg MZE_0.5deg LZE_0.5deg SZE MZE LZE WOBBLE"
-if [[ $1 == *"redHV"* ]]; then
-    PSPACE="SZE_0.5deg MZE_0.5deg LZE_0.5deg"
-fi
+LFIL="$(ls -1 ${1}/*.combined.root)"
+
+CUT=$(basename $1)
+CUT=${CUT/anasum_/}
+echo "CUT: $CUT"
+ODIR="${2}/$CUT"
+echo "Output directory: $ODIR"
+mkdir -p "$ODIR"
+
+PDIR=$(pwd)
+# cd $ROOTSYS
+
+for F in $LFIL; do
+    echo "Analysing $F"
+    TF=$(basename $F .combined.root)
+    TF=${TF/anasum_releaseTesting/}
+    OTF="${ODIR}/${TF}"
+    echo "Output per analysis: $OTF"
+    mkdir -p "${OTF}"
+    # root -q -l -b "plot_energy_spectra.C(\"$F\", \"${OTF}\" )"
+    root -q -l -b "plot_lightcurves.C(\"$F\", \"${OTF}\" )"
+    exit
+done
+
+exit
 
 for N in $PSPACE
 do
-   root -q -l -b "plot_energy_spectra.C(\"${1}\", \"$N\", \"$BCKMODEL\" )"
+   #root -q -l -b "plot_energy_spectra.C(\"${1}\", \"$N\", \"$BCKMODEL\" )"
    root -q -l -b "plot_lightcurves.C(\"${1}\", \"$N\", \"$BCKMODEL\" )"
+#   root -q -l -b "plot_lightcurves.C(\"${1}\", \"$N\", \"$BCKMODEL\" )"
 #   root -q -l -b "plot_skymaps.C(\"${1}\", \"$N\", \"$BCKMODEL\" )"
 #   root -q -l -b "plot_skymaps.C(\"${1}\", \"$N\", \"RB\" )"
 done
