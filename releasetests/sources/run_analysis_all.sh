1eabeb0b722a01df1d61349e647b318f41339e58
GernotMaier <gernot.maier@desy.de>
2024-02-17 17:18:38 +0100

    simplified analysis

--- releasetests/sources/run_analysis_all.sh
+++ releasetests/sources/run_analysis_all.sh
@@ -4,17 +4,14 @@
 #
 if [[ $# -lt 1 ]]; then
 echo "
-    ./run_analysis_all.sh <TYPE>
-
-    analysis types: ANASUM_SUB, ANASUM_FFF
+    ./run_analysis_all.sh
 
 "
 exit
 fi
 
-ANATYPE=${1}
-[[ "$2" ]] && RUNPARA=$2 || RUNPARA="../../../EventDisplay_Release_v490/runparameter/V6.${VERITAS_ANALYSIS_TYPE:0:2}.runparameter.dat"
-
+VERSION="v490"
+ANALYSISTYPE="${VERITAS_ANALYSIS_TYPE:0:2}"
 LTARGETS=$(cat TARGETS.dat)
 
 EPOCHS="V4 V5 V6 all"
@@ -29,19 +26,35 @@ fi
 
 CUTS="hard3tel"
 
+# current directory with run lists
+SDIR=$(pwd)
+# data directory
+DDIR="$VERITAS_USER_DATA_DIR/analysis/Results/${VERSION}/processed_data_v490.7/"
+# output directory
+ODIR="${VERITAS_USER_DATA_DIR}/analysis/Results/${VERSION}/${ANALYSISTYPE}/SourceTests/"
+
+cd ${EVNDISPSCRIPTS} || exit
+
 for T in ${LTARGETS}
 do
     for C in $CUTS
     do
         for E in $EPOCHS
         do
+            if [[ ! -e "${SDIR}/${T}/runlist_releaseTesting_${E}.dat" ]]; then
+                echo "No run list ${SDIR}/${T}/runlist_releaseTesting_${E}.dat"
+                continue
+            fi
+
             echo "Analysing ${T} with ${C} cuts (epoch $E)"
-            ./run_analysis.sh \
-                ${RUNPARA} \
-                ${T} \
-                ${ANATYPE} \
-                ${C} \
-                ${E}
+            echo "   input file list: ${SDIR}/${T}/runlist_releaseTesting_${E}.dat"
+            echo "   input directory: ${DDIR}/${ANALYSISTYPE}/anasum_${C}"
+            echo "   output directory: ${ODIR}/${T}/${E}"
+            continue
+           ./ANALYSIS.anasum_combine.sh \
+               ${SDIR}/${T}/runlist_releaseTesting_${E}.dat \
+               ${DDIR}/${ANALYSISTYPE}/anasum_${C} \
+               ${ODIR}/${T}/${E}
         done
     done
 done
