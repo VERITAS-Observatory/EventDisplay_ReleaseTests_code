9c21a54e2f93357140566782b6f76b69f703491b
GernotMaier <gernot.maier@desy.de>
2024-11-11 09:53:16 +0100

    minor script improvements

--- release_tests/sources/source_tests_summary.sh
+++ release_tests/sources/source_tests_summary.sh
@@ -3,19 +3,24 @@
 # for each object / epoch / cut type
 #
 #
-if [[ $# -lt 3 ]]; then
+if [[ $# -lt 4 ]]; then
 echo "
-    ./source_tests_summary.sh <data dir with anasum log files> <output directory> <new version>
+    ./source_tests_summary.sh <data dir with anasum log files> <directory of last version with results> <output directory> <new version>
 
     Extract results from anasum log files and prepare markdown files for each object / epoch / cut type.
+
+    Builds up on markdown files of previous version (given as second argument).
+
+    Read list of targets from TARGETS.dat
 
 "
 exit
 fi
 
 IDIR="${1}"
-ODIR="${2}"
-VERSION="${3}"
+SDIR="${2}"
+ODIR="${3}"
+VERSION="${4}"
 
 LTARGETS=$(cat TARGETS.dat)
 
@@ -25,25 +30,28 @@ CUTS="softbox"
 # nominal AP and NN cuts
 CUTS="hard2tel soft2tel moderate2tel hard3tel supersoftNN2tel"
 
-# current directory with initial files
-SDIR=$(pwd)
-
 for T in ${LTARGETS}
 do
     for C in $CUTS
     do
         for E in $EPOCHS
         do
-            # Initital result file
+            # Output file
+            OFILE="${ODIR}/${T}/results_${T}_${E}_${C}.md"
+            # Initital result markdown file
             IFILE="${SDIR}/${T}/results_${T}_${E}_${C}.md"
             if [[ ! -e "$IFILE" ]]; then
-                echo "No results file $IFILE"
-                continue
+                echo "No initial results file $IFILE; creating empty file"
+                if [[ ! -e ${OFILE} ]]; then
+                    mkdir -p ${ODIR}/${T}/
+                    echo "## ${T} ${E} ${C}" > "$OFILE"
+                    printf "\`\`\`text\n%s\n%s\n\`\`\`\n" "$RESULT_l1" "$RESULT_l2" >> "$OFILE"
+                fi
             else
                 echo "Initial result file copied to ${ODIR}/${T}/"
                 cp -f "$IFILE" "${ODIR}/${T}/"
             fi
-            # Result
+            # Result from anasum log file
             LFILE="${IDIR}/${T}/${C}/${E}.log"
             if [[ ! -e ${LFILE} ]]; then
                 echo "No anasum log file with results: ${LFILE}"
@@ -53,21 +61,10 @@ do
             RESULT=$(echo "$RESULT" | sed 's/),/),\n/g')
             RESULT_l1=$(echo "$RESULT" | sed -n '1p' | sed "s/(//" | sed "s/)//" | sed "s/^/$VERSION: /")
             RESULT_l2=$(echo "$RESULT" | sed -n '2p' | sed "s/^[[:space:]]*//" | sed "s/^/$VERSION: /")
-            echo $RESULT_l1
-            echo $RESULT_l2
+            echo "RESULT_l1 $RESULT_l1"
+            echo "RESULT_l2 $RESULT_l2"
             # Output result file
-            OFILE="${ODIR}/${T}/results_${T}_${E}_${C}.md"
-            if [[ ! -e ${OFILE} ]]; then
-                mkdir -p ${ODIR}/${T}/
-                {
-                    echo "## ${T} ${E} ${C}"
-
-                    echo "\`\`\`text"
-                    echo "$RESULT_l1"
-                    echo "$RESULT_l2"
-                    echo "\`\`\`"
-                } > ${OFILE}
-            elif grep -q "^$VERSION" "$OFILE"; then
+            if grep -q "^$VERSION" "$OFILE"; then
                 sed -i "" "s#^$VERSION.*#$RESULT_l1#" "$OFILE"
 #                sed -i "" "s#^$VERSION.*#$RESULT_l2#" "$OFILE"
             else
