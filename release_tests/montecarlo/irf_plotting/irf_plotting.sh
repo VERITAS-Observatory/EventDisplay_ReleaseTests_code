39305d55e3f3cf657e962ac6215c56352f4add82
Gernot Maier <gernot.maier@desy.de>
2026-01-12 08:51:44 +0100

    typo

--- release_tests/montecarlo/irf_plotting/irf_plotting.sh
+++ release_tests/montecarlo/irf_plotting/irf_plotting.sh
@@ -18,7 +18,7 @@ CUT="NTel3-PointSource-Hard-TMVA-BDT"
 CUT="NTel2-PointSource-Moderate-TMVA-BDT"
 CUT="NTel2-PointSource-Soft-TMVA-BDT"
 CUT="NTel2-PointSource-Moderate"
-# Comparision plots - version and simtype hardwired
+# Comparison plots - version and simtype hardwired
 COMPAREVERSION="v490"
 COMPARESIMTYPE="CARE_June2020"
 #### (END TEMP FIXED VALUES)
